90af850395a1bc286602937286b155e2d42f741f
Joseph Oravbiere <joravbiere@gmail.com>
2019-05-28 21:18:42 -0400

    Add default text for app

--- scripts/live-markdown-previewer.js
+++ scripts/live-markdown-previewer.js
@@ -6,26 +6,42 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var defaultMarkdown = "# Markdown Previewer Example\n" + "## My Sub Heading\n" + "Here is an example of `inline code`\n" + "```\n# This is an example of a code block\nconsole.log('Hello World');\n```\n" + "Here is a list:\n" + "* Item 1\n* Item 2\n* Item 3" + "\n\nHere's a blockquote:\n" + "> Blockquote\n\n" + "You can make things **bold**\n\n" + "You can even have images:\n\n" + "![Image](https://i.imgur.com/nP0yDKM.jpg)\n\n" + "Learn more about the Markdown language at: " + "[Markdown Language Article](https://en.wikipedia.org/wiki/Markdown)";
+
 var MarkdownEditor = function (_React$Component) {
     _inherits(MarkdownEditor, _React$Component);
 
-    function MarkdownEditor() {
+    function MarkdownEditor(props) {
         _classCallCheck(this, MarkdownEditor);
 
-        return _possibleConstructorReturn(this, (MarkdownEditor.__proto__ || Object.getPrototypeOf(MarkdownEditor)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (MarkdownEditor.__proto__ || Object.getPrototypeOf(MarkdownEditor)).call(this, props));
+
+        _this.state = {
+            input: defaultMarkdown
+        };
+
+        _this.handleInputChange = _this.handleInputChange.bind(_this);
+        return _this;
     }
 
     _createClass(MarkdownEditor, [{
+        key: "handleInputChange",
+        value: function handleInputChange(event) {
+            this.setState({
+                input: event.target.value
+            });
+        }
+    }, {
         key: "render",
         value: function render() {
             var styles = {
                 width: 600,
-                height: 500,
+                height: 900,
                 display: "inline"
             };
             var divStyles = {
                 width: 600,
-                height: 500,
+                height: 900,
                 border: "1px solid black",
                 display: "inline-block",
                 verticalAlign: "top"
@@ -39,8 +55,12 @@ var MarkdownEditor = function (_React$Component) {
             return React.createElement(
                 "div",
                 null,
-                React.createElement("textarea", { id: "editor", style: styles }),
-                React.createElement("div", { id: "preview", style: divStyles, dangerouslySetInnerHTML: renderMarkdown(marked('# Marked in browser')) })
+                React.createElement(
+                    "div",
+                    { style: divStyles },
+                    React.createElement("textarea", { id: "editor", style: styles, value: this.state.input, onChange: this.handleInputChange })
+                ),
+                React.createElement("div", { id: "preview", style: divStyles, dangerouslySetInnerHTML: renderMarkdown(marked(this.state.input)) })
             );
         }
     }]);
--- src/live-markdown-previewer.js
+++ src/live-markdown-previewer.js
@@ -1,13 +1,42 @@
+const defaultMarkdown = "# Markdown Previewer Example\n" +
+                        "## My Sub Heading\n" +
+                        "Here is an example of `inline code`\n" +
+                        "```\n# This is an example of a code block\nconsole.log('Hello World');\n```\n" +
+                        "Here is a list:\n" + 
+                        "* Item 1\n* Item 2\n* Item 3" +
+                        "\n\nHere's a blockquote:\n" +
+                        "> Blockquote\n\n" +
+                        "You can make things **bold**\n\n" +
+                        "You can even have images:\n\n" +
+                        "![Image](https://i.imgur.com/nP0yDKM.jpg)\n\n" +
+                        "Learn more about the Markdown language at: " + "[Markdown Language Article](https://en.wikipedia.org/wiki/Markdown)";
+
+
 class MarkdownEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: defaultMarkdown
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+
     render() {
         const styles = {
             width: 600,
-            height: 500,
+            height: 900,
             display: "inline"
         }
         const divStyles = {
             width: 600,
-            height: 500,
+            height: 900,
             border: "1px solid black",
             display: "inline-block",
             verticalAlign: "top"
@@ -20,8 +49,8 @@ class MarkdownEditor extends React.Component {
 
         return (
             <div>
-                <textarea id="editor" style={styles}></textarea>
-                <div id="preview" style={divStyles} dangerouslySetInnerHTML={renderMarkdown(marked('# Marked in browser'))}></div>
+                <div style={divStyles}><textarea id="editor" style={styles} value={this.state.input} onChange={this.handleInputChange} /></div>
+                <div id="preview" style={divStyles} dangerouslySetInnerHTML={renderMarkdown(marked(this.state.input))}></div>
             </div>
         );
     }
